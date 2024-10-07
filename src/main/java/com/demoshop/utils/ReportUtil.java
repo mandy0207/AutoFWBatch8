5a6b5e02d42bef8f207b25fce07f8f90a30552e1
unknown <mandyatwork24@gmail.com>
2024-10-07 19:41:13 +0530

    reports

--- src/main/java/com/demoshop/utils/ReportUtil.java
+++ src/main/java/com/demoshop/utils/ReportUtil.java
@@ -2,11 +2,12 @@ package com.demoshop.utils;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.demoshop.Context.Constants;
 
 public class ReportUtil {
 
-	public ExtentReports generateReport() {
-		String reportPath= System.getProperty("user.dir")+"//Reports//AutomationReport.html";
+	public static ExtentReports generateReport() {
+		String reportPath= Constants.EXTENTREPORT;
 		ExtentSparkReporter reporter= new ExtentSparkReporter(reportPath);
 		reporter.config().setReportName("Obsqura Automation");
 		reporter.config().setDocumentTitle("Obsqura Zone");
